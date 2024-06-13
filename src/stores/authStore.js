e387f052fc4789791646aab915d928b5473dc3b1
mik135 <michaelsavior1@gmail.com>
2024-06-13 22:39:10 +0100

    Hiding what should be hidden

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -2,5 +2,7 @@ import { createStore } from 'zustand'
 
 export const useAuthStore = createStore((set) => ({
     user: null,
-    setUser: (user) => set({ user })
+    setUser: (user) => set({ user }),
+    isLoggedIn: false,
+    setIsLoggedIn: () => set({ isLoggedIn: !!user })
 }))
--- src/supabase/init.js
+++ src/supabase/init.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://flgxwnsnewokprmrpzux.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsZ3h3bnNuZXdva3BybXJwenV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgyNjQzNDUsImV4cCI6MjAzMzg0MDM0NX0.7Yo9oRszmxqvhdDmJCxR5ySSEdAL7JwhN_r5_Pls3DM'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
